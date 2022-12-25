fd7043771ed01e3e6af0e75d41ddb696d9f36f79
Kirill Shiryaev <infantem.help@gmail.com>
2022-12-26 00:55:36 +0200

    changes

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const app = express();
 const fs = require('fs');
 const mysql = require('mysql');
 const cors = require('cors');
-const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require("express-session");
+var Promise = require('promise');
+const e = require('express');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 require('dotenv').config();
 const port = process.env.PORT || 3100;
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "DELETE"],
+  credentials: true
+}));
 
 
 const connection = mysql.createConnection({
@@ -17,29 +29,239 @@ const connection = mysql.createConnection({
   database: process.env.DB_DATABASE
 });
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
-}))
+
+app.use(
+  session({
+    name: "userID",
+    secret: "GFGEnter", // Secret key,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { httpOnly: false }
+  })
+);
 
 connection.connect((err) => {
   if (err) console.log(err);
   console.log('connected as id ' + connection.threadId);
 });
 
+app.post("/api/sign-in", (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (email && password) { next() } else { res.status(401); res.send("missing parameters") };
+});
+app.post("/api/sign-in", (req, res) => {
+  const { email, password } = req.body;
+  connection.query(`select * from user where email = "${email}" and password = "${password}";`, (err, results) => {
+    if (err) { res.send("error request"); res.statusCode = 403 } else if (results.length < 1) { res.send("user does not exist"); res.statusCode = 402 }
+    else {
+      req.session.userID = results[0];
+      res.status(200);
+      res.send("done");
+    };
+  });
+});
+
+app.post("/api/sign-up", (req, res, next) => {
+  const { first_name, last_name, email, password, birthday } = req.body;
+  if (first_name && last_name && email && password && birthday) { next() } else { res.send("missing parameters").status(405) };
+});
+app.post("/api/sign-up", (req, res, next) => {
+  const { first_name, last_name, email, password, birthday } = req.body;
+  connection.query(`select * from user where email = "${email}" and password = "${password}"`, (err, results) => {
+    if (err) { res.send(err); res.status(401) } else if (results.length < 1) { next() } else { console.log("EXISTS"); res.send("user already exists") };
+  });
+})
+app.post("/api/sign-up", (req, res) => {
+  const { first_name, last_name, email, password, birthday } = req.body;
+  connection.query(`insert into user values(default, "${email}", "${first_name}", "${last_name}", "${password}", "${birthday}");`, (err) => {
+    if (err) {
+      res.status(401).send(err);
+    } else {
+      connection.query(`select * from user where email = "${email}" and password = "${password}"`, (err, results) => {
+        if (err) { res.send(err); res.status(401) }
+        else {
+          req.session.userID = results[0];
+          console.log(req.session)
+          res.status(200);
+          res.send("done");
+        }
+      });
+
+    };
+  });
+});
+
+app.get("/api/cart", (req, res, next) => {
+  if (req.session.userID) {
+    next();
+  } else {
+    res.status(401).send("user not logged in");
+  }
+});
+
+app.get("/api/cart", (req, res) => {
+  const { id } = req.session.userID;
+  connection.query(`
+          select p.id, p.name, p.price, p.images_urls, cd.quantity
+          from cart as c
+          join cart_details as cd on c.id = cd.cart_id
+          join product as p on cd.product_id = p.id 
+          where c.user_id  = ${id};`, (err, results) => {
+    if (err) {
+      res.status(403).send(err);
+      console.log(err);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+
+
+app.get("/api/wishlist", (req, res, next) => {
+  if (req.session.userID) {
+    next();
+  } else {
+    res.status(401).send("user not logged in");
+  }
+});
+
+app.get("/api/wishlist", (req, res) => {
+  const { id } = req.session.userID;
+  connection.query(`select p.id, p.name, p.price, p.images_urls,
+                    p.is_in_storage
+                    from wish_list as w
+                    join wish_list_details as wd on w.id = wish_list_id
+                    join product as p on wd.product_id = p.id 
+                    where w.user_id  = ${id};`, (err, results) => {
+    if (err) {
+      res.status(403).send(err); console.log(err)
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+app.delete("/api/wishlist/:product_id", (req, res, next) => {
+  if (req.session.userID) {
+    next();
+  } else {
+    res.status(401).send("user not logged in");
+  }
+});
+app.delete("/api/wishlist/:product_id", (req, res) => {
+  const { id } = req.session.userID;
+  const product_id = req.params.product_id;
+  connection.query(`
+  delete 
+  from wish_list_details
+  where wish_list_id = (select id
+  from wish_list 
+  where user_id = ${id})
+  and product_id = ${product_id};`,
+    (err, results) => {
+      if (err) {
+        res.status(403).send(err); console.log(err);
+      } else {
+        res.status(200).send("done")
+      }
+    })
+});
+
+
+
+app.post("/api/cart", (req, res, next) => {
+  const { product_id, price, amount } = req.body;
+  if (req.session.userID && product_id && price && amount) {
+    next();
+  } else {
+    res.status(403).send("user not logged in");
+  }
+});
+
+function getTotal(id, res) {
+  connection.query(`select total from cart where user_id = ${id};`, (err, results) => {
+    if (err) { res.status(401).send(err) } else {
+      return Number(results[0]);
+    }
+  });
+}
+
+function getCartID(id, res) {
+  return new Promise((resolve, reject) => {
+    connection.query(`select id from cart where user_id = ${id};`, (err, results) => {
+      if (err) { res.status(401).send(err); reject() } else {
+        const result = results[0] ? results[0] : { id: -1 };
+        resolve(result);
+      }
+    });
+  })
+}
+
+
+app.post("/api/cart", (req, res) => {
+  const { id } = req.session.userID;
+  const { product_id, price, amount } = req.body;
+  connection.query(`select * from cart where user_id = ${id};`, (err, results) => {
+    if (err) { res.status(401).send(err) } else if (results.length < 1) {
+      connection.query(`insert into cart values(default, ${id}, ${Number(price) * Number(amount)});`, (err) => {
+        if (err) { res.status(401).send(err) } else {
+          const cartID = getCartID(id, res);
+          connection.query(`insert into cart_details values(${cartID}, ${product_id}, ${amount}, 0);`, (err) => {
+            if (err) { res.status(401).send(err) }
+          });
+          res.status(200).send("done");
+        }
+      });
+    } else {
+      let total = getTotal(id, res);
+      connection.query(`update cart set total = ${total + Number(price) * Number(amount)} where user_id = ${id};`, (err) => {
+        if (err) { res.status(403).send(err) } else { res.status(200).send("done") }
+      });
+    }
+  });
+
+
+});
+
+
+app.get("/api/sign-out", (req, res) => {
+  req.session.destroy();
+  res.status(200).send("done");
+});
+
+
+
 
 app.get('/api/categories', (req, res) => {
-  connection.query(`select * from category;`, (err, results, fields) => {
+  connection.query(`select * from category;`, (err, results) => {
     if (err) res.send(err);
+
     res.send(results);
   });
 });
 
 app.get('/api/products', (req, res) => {
-  connection.query(`select * from product;`, (err, results, fields) => {
+  connection.query(`select * from product;`, (err, results) => {
     if (err) res.send(err);
     res.send(results);
   })
+});
+app.get('/api/products-ids', (req, res) => {
+  connection.query(`select id from product;`, (err, results) => {
+    if (err) res.send(err);
+    const ids = results.map((item) => item.id);
+    res.send(ids);
+  })
+});
+
+app.get('/api/top-products', (req, res) => {
+  connection.query(`select * from product order by rating desc limit 5;`, (err, results) => {
+    if (err) { res.send(err); res.status(401) } else {
+      res.send(results);
+    }
+  });
 });
 
 app.get('/api/top-products/:id', (req, res, next) => {
@@ -47,9 +269,8 @@ app.get('/api/top-products/:id', (req, res, next) => {
   if (!isNaN(id)) { next() } else { res.send("id is not a number") };
 });
 app.get('/api/top-products/:id', (req, res) => {
-  connection.query(`select * from product  where category_id = ${req.params.id} limit 4;`, (err, results, fields) => {
+  connection.query(`select * from product  where category_id = ${req.params.id} limit 4;`, (err, results) => {
     if (err) res.send(err);
-    console.log(results);
     res.send(results);
   })
 });
@@ -59,8 +280,8 @@ app.get('/api/products/:id', (req, res, next) => {
 });
 app.get('/api/products/:id', (req, res) => {
   const id = req.params.id;
-  connection.query(`select * from product where id=${id};`, (err, results, fields) => {
-    if (err) { res.send(err); res.statusCode = 401 } else { res.send(results) };
+  connection.query(`select * from product where id=${id};`, (err, results) => {
+    if (err) { res.send(err); res.statusCode = 401 } else { res.send(results[0]) };
   })
 });
 app.get('/api/categories/:id', (req, res, next) => {
@@ -69,7 +290,7 @@ app.get('/api/categories/:id', (req, res, next) => {
 });
 app.get('/api/categories/:id', (req, res) => {
   const id = req.params.id;
-  connection.query(`select * from category where id=${id};`, (err, results, fields) => {
+  connection.query(`select * from category where id=${id};`, (err, results) => {
     if (err) { res.send(err); res.statusCode = 401 } else { res.send(results) };
   })
 });
@@ -79,10 +300,11 @@ app.get('/api/products/categories/:id', (req, res, next) => {
 });
 app.get('/api/products/categories/:id', (req, res) => {
   const id = req.params.id;
-  connection.query(`select * from product where category_id=${id};`, (err, results, fields) => {
+  connection.query(`select * from product where category_id=${id};`, (err, results) => {
     if (err) { res.send(err); res.statusCode = 401 } else { res.send(results) };
   })
 });
+
 
 app.listen(port);
 
