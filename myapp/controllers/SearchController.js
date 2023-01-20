3374cdb3d6c6f39fa9738fb4bdbc93543843eeff
Kirill Shiryaev <infantem.help@gmail.com>
2023-01-20 02:41:57 +0200

    extend search controller

--- myapp/controllers/SearchController.js
+++ myapp/controllers/SearchController.js
@@ -1,43 +1,43 @@
 const Category = require("../models/Category");
 const Products = require("../models/Product");
+const { Op } = require("sequelize");
 
 module.exports = {
   async searchCategories(req, res) {
     try {
       const request = req.params.request;
-      const categories = await Category.find({
-        name: {
-          $regex: request,
-          $options: "i"
+      const categories = await Category.findAll({
+        where: {
+          name: { [Op.like]: `${request}%` }
         }
       })
       return categories;
     } catch (e) {
       console.log(e);
-      res.status(500).send("Server error");
+      return e;
     }
 
   },
   async searchProducts(req, res) {
     try {
       const request = req.params.request;
-      const products = await Products.find({
-        name: {
-          $regex: request,
-          $options: "i"
+      const products = await Products.findAll({
+        where: {
+          name: { [Op.like]: `%${request}%` }
         }
       })
+      console.log("products", products)
       return products;
     } catch (e) {
       console.log(e);
-      res.status(500).send("Server error");
+      return e;
     }
 
   },
   async search(req, res) {
     try {
-      const resultCategory = this.searchCategories(req, res);
-      const resultProducts = this.searchProducts(req, res);
+      const resultCategory = await this.searchCategories(req, res);
+      const resultProducts = await this.searchProducts(req, res);
       res.status(200).send({ categories: resultCategory, products: resultProducts });
     } catch (e) {
       console.log(e);
